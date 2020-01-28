bae6dbb362d17a2656c0b7296b418088a5f479bd
John <jlo@cyberratss.london>
2020-01-28 16:53:28 +0000

    added recipe serializer

--- app/core/models.py
+++ app/core/models.py
@@ -75,6 +75,6 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
-    
+
     def __str__(self):
         return self.title
